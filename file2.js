0a302850534c7d9802eac4e2dfe6dff9947ffa97
Chanchal Sekwadiya <129304493+Chanchal2505@users.noreply.github.com>
2024-08-04 22:36:22 +0530

    Add files via upload

---
+++ file2.js
@@ -0,0 +1,11 @@
+// Guess Number Game
+
+let gameNum=25;
+let userNum=prompt("Guess the game number");
+
+while(userNum != gameNum)
+{
+    userNum=prompt("you entered wrong number.Guess again");
+}
+
+console.log("Congratulations, you entered the right number");
